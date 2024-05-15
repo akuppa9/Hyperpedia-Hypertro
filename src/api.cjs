83f7a8b64825ff02d23076b7763cd6e46d58639c
akuppa9 <amogh.kuppa9@gmail.com>
2024-05-14 20:40:48 -0400

    change port binding

--- src/api.cjs
+++ src/api.cjs
@@ -9,6 +9,6 @@ app.get("/:prompt", cors(), function (req, res) {
     res.end(data);
   });
 });
-var server = app.listen(10000, function () {
-  console.log("Express App running at http://127.0.0.1:10000/");
+var server = app.listen(10000, "0.0.0.0", function () {
+  console.log("Express App running at http://0.0.0.0:10000/");
 });
