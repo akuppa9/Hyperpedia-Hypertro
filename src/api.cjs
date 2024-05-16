b896ed00a790ecda6d2fd2c7eb7b446cbc606b60
akuppa9 <amogh.kuppa9@gmail.com>
2024-05-15 22:47:51 -0400

    change port

--- src/api.cjs
+++ src/api.cjs
@@ -3,12 +3,13 @@ var express = require("express");
 var cors = require("cors");
 var app = express();
 var fs = require("fs");
+const port = process.env.PORT || 10000;
 const hypertro = require("./Hypertro.cjs");
 app.get("/:prompt", cors(), function (req, res) {
   hypertro.run(req.params.prompt).then((data) => {
     res.end(data);
   });
 });
-var server = app.listen(10000, "0.0.0.0", function () {
-  console.log("Express App running at http://0.0.0.0:10000/");
+var server = app.listen(port, "0.0.0.0", function () {
+  console.log("Express App running at http://0.0.0.0:"+port);
 });
