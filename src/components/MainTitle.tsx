d44f97f69ffb96179402733a7b752ad5bf8c3d23
akuppa9 <amogh.kuppa9@gmail.com>
2024-05-16 16:43:05 -0400

    home responsive main page

--- src/components/MainTitle.tsx
+++ src/components/MainTitle.tsx
@@ -1,30 +1,33 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+
 const MainTitle = () => {
   const navigate = useNavigate();
   return (
     <>
-      <div className="text-white mt-40 ml-8 ">
-        <h1 className="text-7xl font-bold">The Hyperpedia</h1>
-        <br></br>
-        <h1 className="text-6xl font-light">The Ultimate Gym Encyclopedia</h1>
+      <div className="text-white mt-20 ml-4 sm:mt-40 sm:ml-8">
+        <h1 className="text-4xl sm:text-7xl font-bold">The Hyperpedia</h1>
+        <br />
+        <h1 className="text-3xl sm:text-6xl font-light">
+          The Ultimate Gym Encyclopedia
+        </h1>
       </div>
-      <div className="flex flex-col md:flex-row mt-10 ml-8 space-y-4 md:space-y-0 md:space-x-4">
+      <div className="flex flex-col md:flex-row mt-10 ml-4 sm:ml-8 space-y-4 md:space-y-0 md:space-x-4">
         <div>
           <a
             href="#"
-            className=" font-medium text-3xl bg-gradient-to-l from-cyan-500 to-blue-500 border-blue-400 rounded-full py-3 px-8 hover:bg-gradient-to-bl text-white inline-block hover:border-blue-400 hover:text-white duration-300 hover:border border border-transparent"
+            className="font-medium text-xl sm:text-3xl bg-gradient-to-l from-cyan-500 to-blue-500 border-blue-400 rounded-full py-2 sm:py-3 px-6 sm:px-8 hover:bg-gradient-to-bl text-white inline-block hover:border-blue-400 hover:text-white duration-300 hover:border border border-transparent"
             onClick={() => {
               navigate("/hypertrophy");
             }}
           >
-            What is Hypertophy?
+            What is Hypertrophy?
           </a>
         </div>
         <div>
           <a
             href="#"
-            className="ml-1 font-medium text-3xl border border-blue-500 bg-black rounded-full py-3 px-8 hover:bg-gradient-to-l from-cyan-500 to-blue-500 text-white inline-block hover:border-blue-400 hover:text-white duration-300 hover:border"
+            className="ml-1 font-medium text-xl sm:text-3xl border border-blue-500 bg-black rounded-full py-2 sm:py-3 px-6 sm:px-8 hover:bg-gradient-to-l from-cyan-500 to-blue-500 text-white inline-block hover:border-blue-400 hover:text-white duration-300 hover:border"
             onClick={() => {
               navigate("/body-model");
             }}
